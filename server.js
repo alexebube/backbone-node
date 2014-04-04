87edd9390065817f986bf50b24ea8b7378b76ecf
Alexander Obi <alexebube@gmail.com>
2014-04-03 22:27:57 -0400

    Added initial files

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 "use strict";
 
-var application_root = __dirname,
-	express = require( 'express' ), //Web framework
-	path = require( 'path' ); //Utilities for dealing with file paths
+var application_root = __dirname;
+var express = require('express'); //Web framework
+var path = require('path'); //Utilities for dealing with file paths
 
 //Create server
 var app = express();
 
 // Configure server
-app.configure( function() {
+app.configure(function () {
 	//parses request body and populates request.body
-	app.use( express.bodyParser() );
+	app.use(express.bodyParser());
 	//checks request.body for HTTP method overrides
-	app.use( express.methodOverride() );
+	app.use(express.methodOverride());
 	//perform route lookup based on URL and HTTP method
-	app.use( app.router );
+	app.use(app.router);
 	//Where to serve static content
-	app.use( express.static( path.join( application_root, 'site') ) );
+	app.use(express.static(path.join(application_root, 'site')));
 	//Show all errors in development
-	app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 //Start server
-var port = 4000;
-app.listen( port, function() {
-	console.log( 'Express server listening on port %d in %s mode', 
-	port, app.settings.env );
+var port = 5000;
+app.listen(port, function () {
+	console.log('Express server listening on port %d in %s mode', port, app.settings.env);
 });
